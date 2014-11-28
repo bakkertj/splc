aa9a4feb5467b2a0f38f96c4b6953b00648adbd1
Trevor Bakker <tbakker@gmail.com>
2014-11-27 23:00:40 -0600

    Initial Commit

---
+++ spl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  spl
+//
+//  Created by Trevor Bakker on 11/27/14.
+//  Copyright (c) 2014 __TREVORBAKKER__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
